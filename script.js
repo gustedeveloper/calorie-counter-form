b91ba9c860872df2e90747bba8cc777f90d9f206
gustedeveloper <219.guste@gmail.com>
2024-04-04 13:33:26 +0200

    Implement function to add new entry inputs dynamically based on user selection

--- script.js
+++ script.js
@@ -6,12 +6,23 @@ const clearButton = document.getElementById('clear');
 const output = document.getElementById('output');
 let isError = false;
 
+
+// Function to clean input string by removing any non-numeric characters
 function cleanInputString(str) {
     const regex = /[+-\s]/g;
     return str.replace(regex, "");
 }
 
+// Function to check if input string contains invalid exponential notation
 function isInvalidInput(str) {
     const regex = /\d+e\d+/i;
     return str.match(regex);
 }
+
+function addEntry() {
+    const targetInputContainer = document.querySelector(`#${entryDropdown.value} .input-container`);
+    const entryNumber = targetInputContainer.querySelectorAll('input[type="text"]').length;
+    const HTMLString = `
+    <label for="${entryDropdown.value}-${entryNumber}-name">Entry ${entryNumber} Name</label>
+    <input type="text" id="${entryDropdown.value}-${entryNumber}-name" placeholder="Name" />`;
+  }
