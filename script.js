d7496b7bda693f179098f4342a48ca2dcb8551c3
gustedeveloper <219.guste@gmail.com>
2024-04-08 10:00:46 +0200

    Improve 'addEntry' function to dynamically generate input fields for entry name and calories based on user selection, and attach it to the 'addEntryButton' event listener

--- script.js
+++ script.js
@@ -21,8 +21,14 @@ function isInvalidInput(str) {
 
 function addEntry() {
     const targetInputContainer = document.querySelector(`#${entryDropdown.value} .input-container`);
-    const entryNumber = targetInputContainer.querySelectorAll('input[type="text"]').length;
+    const entryNumber = targetInputContainer.querySelectorAll('input[type="text"]').length + 1;
     const HTMLString = `
     <label for="${entryDropdown.value}-${entryNumber}-name">Entry ${entryNumber} Name</label>
-    <input type="text" id="${entryDropdown.value}-${entryNumber}-name" placeholder="Name" />`;
-  }
+    <input type="text" id="${entryDropdown.value}-${entryNumber}-name" placeholder="Name" />
+    <label for="${entryDropdown.value}-${entryNumber}-calories">Entry ${entryNumber} Calories</label>
+    <input type="number" id="${entryDropdown.value}-${entryNumber}-calories" min="0" placeholder="Calories" />`
+    targetInputContainer.insertAdjacentHTML('beforeend', HTMLString);
+}
+
+
+addEntryButton.addEventListener("click", addEntry);
